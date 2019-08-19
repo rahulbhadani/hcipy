0dfc9d282d88caf53f52fd0720f632ca38457b00
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2019-08-18 22:10:06 -0700

    Now Applying Random gains to actuator in every step

--- ao_examples/AO_EXAMPLE_2.py
+++ ao_examples/AO_EXAMPLE_2.py
@@ -6,7 +6,7 @@
 # 
 # First let's import HCIPy, and a few supporting libraries:
 
-# In[1]:
+# In[24]:
 
 
 import sys
@@ -22,7 +22,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # HCIPy implements a multi-layer atmospheric model. Each layer derives from `AtmosphericLayer`. This allows for multiple implementations of phase-screen calculation. One of the implemented methods is an infinite phase-screen extrusion method by Assemat et al. 2006. We first do some setup work.
 
-# In[2]:
+# In[25]:
 
 
 WAVELENGTH = 500e-9 # Wavelength
@@ -39,26 +39,32 @@ plt.title('Circular Aperture')
 
 # We can now construct the layer.
 
-# In[3]:
+# In[26]:
 
 
 # Reference Flat Wavefront
 wf_ref = Wavefront(aperture_grid, WAVELENGTH)
 wf_ref.total_power = 1
 
-imshow_field(wf_ref.phase, vmin=-np.pi, vmax=np.pi, cmap='RdBu'); 
+imshow_field(wf_ref.phase*aperture_grid, vmin=-np.pi, vmax=np.pi, cmap='RdBu'); 
 plt.title('Reference Wavefront')
 plt.colorbar()
 
 
-# In[30]:
+# In[27]:
 
 
-fried_parameter = 0.2 # meter
+fried_parameter = 0.15 # meter
 outer_scale = 20 # meter
 velocity = 10 # meter/sec
 Cn_squared = Cn_squared_from_fried_parameter(fried_parameter, 500e-9)
 
+
+'''
+WHY IS THIS STEP SO SLOW. IT WILL BE REALLY DIFFICULT TO SIMULATE CHANGING TURBULENCE DUE TO WIND
+AND APPLY DM CORRECTION IF THE CODE EXECUTION IS SO SLOW AT THIS STEP.
+
+'''
 # Make multi-layer atmosphere
 layers = make_standard_atmospheric_layers(pupil_grid, outer_scale)
 atmos = MultiLayerAtmosphere(layers, scintilation=True)
@@ -68,7 +74,7 @@ atmos.Cn_squared = Cn_squared
 atmos.reset()
 
 
-# In[31]:
+# In[34]:
 
 
 wf_atmos = atmos(wf_ref)
@@ -80,13 +86,20 @@ plt.colorbar()
 plt.show()
 
 
-# In[32]:
+# In[35]:
 
 
-layers[0].t = 2.5
-layers[1].t = 2.5
-layers[3].t = 2.5
+layers[0].t = 2.5 # SECONDS
+layers[1].t = 2.5 # SECONDS
+layers[3].t = 2.5 # SECONDS
 wf_atmos = atmos(wf_ref)
+
+'''
+WHY IS THIS STEP SO SLOW. IT WILL BE REALLY DIFFICULT TO SIMULATE CHANGING TURBULENCE DUE TO WIND
+AND APPLY DM CORRECTION IF THE CODE EXECUTION IS SO SLOW AT THIS STEP.
+
+'''
+
 atmos = MultiLayerAtmosphere(layers, scintilation=True)
 
 # Show scintillation field
@@ -96,7 +109,7 @@ plt.colorbar()
 plt.show()
 
 
-# In[43]:
+# In[36]:
 
 
 # Now we want to propagate to focal_plane
@@ -128,7 +141,7 @@ wf_prop_rms = np.sqrt(np.mean(np.square(wf_prop.phase))-
 print('RMS Wavefront Error of turbulent wavefron is {}'.format(wf_prop_rms))
 
 
-# In[ ]:
+# In[37]:
 
 
 ###############################################################
@@ -138,10 +151,17 @@ print('RMS Wavefront Error of turbulent wavefron is {}'.format(wf_prop_rms))
 # (Taken from Wavefront_sensing_1.ipynb)
 # 
 
-# In[44]:
+# In[38]:
 
 
-F_mla = 40. / 0.3
+#SquareShackHartmannWavefrontSensorOptics(
+#    input_grid,
+#    f_number,
+#    num_lenslets,
+#    pupil_diameter,
+#)
+
+F_mla = 40. / 0.3 # F- NUMBER
 N_mla = 5 # number of subapertures across the lenslet array
 
 shwfs = SquareShackHartmannWavefrontSensorOptics(focal_grid, F_mla, N_mla, min_detector_size*1.05)
@@ -151,7 +171,7 @@ shwfse = ShackHartmannWavefrontSensorEstimator(shwfs.mla_grid, shwfs.micro_lens_
 # ## Deformable Mirror with which we need to calibrate our wavefront sensor
 # Our deformable mirror in this example is modeled as a Zernike freeform surface where each mode is a Zernike polynomial.
 
-# In[53]:
+# In[39]:
 
 
 # DM slightly larger than the detector plane for better edge performance
@@ -159,6 +179,13 @@ actuator_grid = make_pupil_grid(10, min_detector_size*1.1)
 sigma = min_detector_size/10.
 gaussian_basis = make_gaussian_pokes(focal_grid, actuator_grid, sigma)
 dm = DeformableMirror(gaussian_basis)
+
+'''
+IT IS NOT CLEAR BY num_modes is 100 here.
+
+IS THERE A WAVE TO PLOT DM TO SEE WHERE ON DM ACTUATORS ARE LOCATED?
+'''
+
 num_modes = len(dm.influence_functions)
 dm.actuators = np.zeros(num_modes)
 
@@ -166,7 +193,7 @@ dm.actuators = np.zeros(num_modes)
 # 
 # The calibration process is as follows: we "poke" each mode of the DM with an equal positive and a negative amplitude successively, measure the shifts in the centroids of the microlens array images, and use these measurements to construct an interaction matrix.
 
-# In[70]:
+# In[40]:
 
 
 # First set the amplitudes of all DM modes to zero, 
@@ -174,7 +201,11 @@ dm.actuators = np.zeros(num_modes)
 
 wf_dmref = Wavefront(detector_grid, WAVELENGTH)
 
+'''
 
+NOT SURE IF WAVEFRONT FOR CALIBRATIN DM SHOULD BE UNABERRATED REFERENCE WAVEFRONT
+
+'''
 img = shwfs.forward(wf_dmref).power
 ref = shwfse.estimate([img]).ravel()
 num_measurements = ref.shape[0]
@@ -219,7 +250,7 @@ Intmat = ModeBasis(Intmat)
 
 # To reconstruct a wavefront, we need the control matrix which is obtained by inverting the interaction matrix. We use the singular value decomposition functionality offered to us by hcipy to obtain a pseudo-inverse of this matrix, cut off the singular values that are below a threshold.
 
-# In[71]:
+# In[41]:
 
 
 control_mat = inverse_tikhonov(Intmat.transformation_matrix, rcond=1e-7)
@@ -227,21 +258,32 @@ control_mat = inverse_tikhonov(Intmat.transformation_matrix, rcond=1e-7)
 
 # With this information, we can reconstruct a wavefront using the SH-WFS centroid measurements it produces, on the modal basis of the DM. Let's see how well we can reconstruct the atmospheric turbulence phase screen from earlier.
 
-# In[79]:
+# In[44]:
 
 
 loops = 225 # Experiment with this to find the best number of loops to flatten the wavefront
-gain = 0.1 # Experiment with this to find a reasonable value
+gain = np.random.rand(num_modes) # Experiment with this to find a reasonable value
 
 # Reset the DM modes to zero amplitude
 dm.actuators = np.zeros(dm.actuators.shape)
 
+
+# Assuming for the very first time, wavefront sensed by 
+## WFS is abberrated one (as DM is flat in the beginning)
+corrected_wf = wf_prop 
+
+
 for loop in np.arange(loops):
     # Propagate the wavefront through the WFS optics
-    dm_wf = dm.forward(wf_prop)
+    
+    ## Propagate the aberrated wavefront through dm
+    dm_wf = dm.forward(corrected_wf)
+    ## Propagate wavefron from dm to Wavefront sensor.
     sh_wf = shwfs.forward(dm_wf)
     sh_img = sh_wf.power
 
+    
+    ## Control Task
     # Estimate
     meas_vec = shwfse.estimate([sh_img])
     meas_vec = meas_vec.ravel()
@@ -251,18 +293,25 @@ for loop in np.arange(loops):
     change_in_actuators -= change_in_actuators.mean() # note that we do this to remove piston errors that creep in
     dm.actuators -= gain * change_in_actuators
     
-    flattened_wf = dm.forward(wf_prop)
+    ## Assuming DM is fast enough to act that wavefront has not
+    ## changed much during DM adjusts its actuator based on WFS feedback
+    
+    '''
+    WELL, THIS DOESN'T REALLY SEEM TO DO ANYTHING, AS CAN BE SEEN FROM EITHER INTENSITY PLOT OR PHASE PLOT BELOW.
+    '''
+    corrected_wf = dm.forward(corrected_wf)
     #wf_rms_error = np.sqrt(np.mean(np.square(wf_prop.phase)))
     ## Calculate the wavefront's root mean sqaure error
-    wf_prop_rms = np.sqrt(np.mean(np.square(flattened_wf.phase))-
+    wf_prop_rms = np.sqrt(np.mean(np.square(corrected_wf.phase))-
                            
-                           np.square(np.mean(flattened_wf.phase)))
+                           np.square(np.mean(corrected_wf.phase)))
 
-    imshow_field(np.log10(flattened_wf.intensity/flattened_wf.intensity.max())*detector_grid, cmap='RdBu')
-
-    plt.title("Flattened wavefront after {0} loops, with RMS WF error of {1} radians".format(loops, wf_prop_rms))
-    plt.draw()
-    plt.pause(0.01)
+    if loop%15 == 0:
+       # imshow_field(np.log10(corrected_wf.intensity/corrected_wf.intensity.max())*detector_grid, cmap='RdBu')
+        imshow_field(corrected_wf.phase, cmap='RdBu')
+        plt.title("Flattened wavefront after {0} loops, with RMS WF error of {1} radians".format(loop, wf_prop_rms))
+        plt.draw()
+        plt.pause(0.01)
 
 
 # In[ ]:
